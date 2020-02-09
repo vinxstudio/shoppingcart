0c0c2ca3b47395a3f51943b686176b0f596026be
vinxstudio <vinxstudio@gmail.com>
2020-02-09 13:50:07 +1100

    Changes on Cart User Interface to adopt on Mobile.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,10 +4,9 @@ import { removeItem } from "./actions/cartActions";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import MenuList from "@material-ui/core/MenuList";
-import MenuItem from "@material-ui/core/MenuItem";
 import Grid from "@material-ui/core/Grid";
 import { Divider } from "@material-ui/core";
+import ButtonBase from "@material-ui/core/ButtonBase";
 
 class Cart extends Component {
   handleRemove = productName => {
@@ -18,34 +17,44 @@ class Cart extends Component {
     let addedItems = this.props.items.length ? (
       this.props.items.map(item => {
         return (
-          <MenuItem key={item.productName}>
-            <img src={item.productImage} alt={item.productName} height="100" />
-            <Grid >
-              <Typography style={{ paddingLeft: 10 }}>
-                {item.productName}
-              </Typography>
-              <div style={{ whiteSpace: "pre-wrap" }}></div>
-              <Typography
-                variant="body2"
-                color="textSecondary"
-                component="p"
-                style={{ paddingLeft: 10 }}
-              >
-                ${item.price}
-              </Typography>
-
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => {
-                  this.handleRemove(item.productName);
-                }}
-                style={{ marginLeft: 10, marginTop: 10, textAlign: "right" }}
-              >
-                Remove
-              </Button>
+          <Grid container spacing={2} key={item.productName}>
+            <Grid item>
+              <ButtonBase>
+                <img
+                  height="100"
+                  src={item.productImage}
+                  alt={item.productName}
+                />
+              </ButtonBase>
             </Grid>
-          </MenuItem>
+            <Grid item xs={12} sm container>
+              <Grid
+                container
+                direction="column"
+                spacing={2}
+                style={{ marginTop: 10, marginLeft: 1 }}
+              >
+                <Typography>{item.productName} </Typography>
+
+                <Typography variant="body2" color="textSecondary">
+                  ${item.price}
+                </Typography>
+
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                    onClick={() => {
+                      this.handleRemove(item.productName);
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         );
       })
     ) : (
@@ -53,14 +62,15 @@ class Cart extends Component {
         There are no items in your shopping cart.
       </Typography>
     );
+
     return (
       <div style={{ marginTop: 10 }}>
-        <Paper elevation={1} style={{ padding: 20 }}>
+        <Paper elevation={1} style={{ padding: 10 }}>
           <Typography gutterBottom variant="h6" component="h2">
             Shopping Cart
           </Typography>
           <Divider />
-          <MenuList>{addedItems} </MenuList>
+          <div style={{ flexGrow: 1, marginTop: 5 }}>{addedItems} </div>
         </Paper>
       </div>
     );
